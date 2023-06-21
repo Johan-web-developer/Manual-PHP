6cbe2ed8443df336579dabcddb3e8873d5f29446
Johan-web-developer <johan.flore.13@gmail.com>
2023-06-21 12:51:57 -0500

    añadidas funciones definidas

--- Funciones-Definidas-porUsuario.php
+++ Funciones-Definidas-porUsuario.php
@@ -8,9 +8,24 @@
 
 
 
-declare(strict_types=1);
+/* declare(strict_types=1);
 function sumar(int $numero1 = 0, array $numero2 ) {
     echo "$numero1 + $numero2";
 }
 sumar (10, []);
+?> */
+
+
+declare(strict_types=1);
+//include 'includes/header.php';
+function usuarioAutenticado(bool $autenticado) : ?string {
+    if($autenticado){
+        return "El usuario esta autenticado";
+    }else{
+        return null;
+    }
+}
+$usuario = usuarioAutenticado(false);
+echo $usuario;
+//include 'includes/footer.php';
 ?>
